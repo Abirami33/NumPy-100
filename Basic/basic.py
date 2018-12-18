9128f939645e3f098fe04dd2c074121a55431554
Abirami33 <38695790+Abirami33@users.noreply.github.com>
2018-12-18 13:02:11 +0530

    Create basic.py

---
+++ Basic/basic.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+#base is a multi dimensional array containing 2 arrays
+base=np.array([[2,3,4],['Raj','Ram','Yak']])
+
+print(base)
+
+print(type(base))
+
+
+#OUTPUT:
+'''
+stud@HP-246-Notebook-PC:~$ python basic.py
+[['2' '3' '4']
+ ['Raj' 'Ram' 'Yak']]
+<class 'numpy.ndarray'>
+
+'''
