1202f93cbe46b638e2ef621abbeed5dccb7d1ee1
Abirami33 <38695790+Abirami33@users.noreply.github.com>
2018-12-18 13:07:43 +0530

    Update basic.py

--- Basic/basic.py
+++ Basic/basic.py
@@ -7,6 +7,14 @@ print(base)
 
 print(type(base))
 
+print("Dimension is: " , base.ndim)
+
+print("Shape is: ",base.shape)
+
+print("Size is: ", base.size)
+
+print("Data type object is: ", base.dtype)
+
 
 #OUTPUT:
 '''
@@ -14,5 +22,9 @@ stud@HP-246-Notebook-PC:~$ python basic.py
 [['2' '3' '4']
  ['Raj' 'Ram' 'Yak']]
 <class 'numpy.ndarray'>
+Dimension is:  2
+Shape is:  (2, 3)
+Size is:  6
+Data type object is:  <U11
 
 '''
