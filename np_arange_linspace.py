1959a5e069a8dbd1efc7d76fbdaa2bbe9a171471
Abirami33 <38695790+Abirami33@users.noreply.github.com>
2018-12-19 13:19:14 +0530

    Update np_arange_linspace.py

--- np_arange_linspace.py
+++ np_arange_linspace.py
@@ -4,6 +4,8 @@ import numpy as np
 b3=np.arange(1, 10, 2)
 print(b3)
 
+#NOTE: Arange is Similar to linspace, but uses a step size (instead of the number of samples).
+
 #prints 7 values between 1 and 10
 b4=np.linspace(1, 10, 7)
 print(b4)
