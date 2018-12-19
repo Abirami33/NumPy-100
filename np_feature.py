50b204e32d48ef62646de11401393cb82b2ea216
Abirami33 <38695790+Abirami33@users.noreply.github.com>
2018-12-19 13:04:36 +0530

    Add files via upload

---
+++ np_feature.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+base=np.array([[2,3,4],['Raj','Ram','Yak']])
+
+print("Dimension is: " , base.ndim)
+
+print("Shape is: ",base.shape)
+
+print("Size is: ", base.size)
+
+print("Data type object is: ", base.dtype)
+
+#OUTPUT:
+'''
+Dimension is:  2
+Shape is:  (2, 3)
+Size is:  6
+Data type object is:  <U11
+'''
