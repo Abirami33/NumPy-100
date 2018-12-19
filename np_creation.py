b20fd657d16af7ad940ff273a0261d9efe2f02e4
Abirami33 <38695790+Abirami33@users.noreply.github.com>
2018-12-19 13:04:19 +0530

    Add files via upload

---
+++ np_creation.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#base is a multi dimensional array containing 2 arrays
+base=np.array([[2,3,4],['Raj','Ram','Yak']])
+
+print(base)
+
+print(type(base))
+
+
+
+#OUTPUT:
+'''
+stud@HP-246-Notebook-PC:~$ python np_creation.py
+[['2' '3' '4']
+ ['Raj' 'Ram' 'Yak']]
+<class 'numpy.ndarray'>
+
+'''
